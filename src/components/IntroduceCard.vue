ab1a0a697fa4b8ea88c05eab5ab5af49ef8be735
spooder <yoshi7591@naver.com>
2022-11-25 15:57:37 +0900

    second commit

---
+++ src/components/IntroduceCard.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img :src="require('@/assets/' + imglnk)" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img :src="require('@/assets/' + imglnk)" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">{{ name }}</p>
+        <p class="subtitle is-6">{{ snsid }}</p>
+      </div>
+    </div>
+
+    <div class="content">
+      <p>{{ description }}</p>
+      <time datetime="2016-1-1">- RD Corp</time>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped src="@/css/bulma.css">
+</style>
+
+<script>
+    export default {
+        name: 'IntroduceCard',
+        props: {
+            name: String,
+            snsid: String,
+            description: String,
+            imglnk: String
+        }
+    }
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="introduce">
+    <p class="title">제작에 참여한 팀원을 소개합니다.</p>
+  </div>
+  <div class="contents">
+    <Card imglnk="건희프로필.jpg" name="조건희" snsid="@spooder02" description="프론트/백엔드 개발자입니다. 항상 최선을 다하겠습니다!"/>
+    <Card imglnk="도형프로필.jpg" name="김도형" snsid="@travis04" description="여친 급구"/>
   </div>
 </template>
+  
+<style scoped>
+  .contents {
+    display: flex;
+    justify-content: space-around;
+  }
+  p.title {
+    font-weight: 700;
+    font-size: 30pt;
+    text-align: center;
+  }
+</style>
+  
+<script>
+  import Card from '@/components/IntroduceCard.vue';
+  export default {
+    name: 'AboutView',
+    components: {
+      Card
+    }
+  }
+</script>
